c774f0e6d6307eb97ad53bee886c5ce92c987cf4
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2025-10-27 14:50:59 -0300

    update composer

--- bootstrap/cache/services.php
+++ bootstrap/cache/services.php
@@ -30,8 +30,6 @@
     26 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
     27 => 'Termwind\\Laravel\\TermwindServiceProvider',
     28 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
-    29 => 'App\\Providers\\AppServiceProvider',
-    30 => 'App\\Providers\\RouteServiceProvider',
   ),
   'eager' => 
   array (
@@ -50,8 +48,6 @@
     12 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
     13 => 'Termwind\\Laravel\\TermwindServiceProvider',
     14 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
-    15 => 'App\\Providers\\AppServiceProvider',
-    16 => 'App\\Providers\\RouteServiceProvider',
   ),
   'deferred' => 
   array (
