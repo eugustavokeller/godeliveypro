ef1e57c5437f8a9e25f9663ddca153e5761c26c2
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2025-10-27 22:16:13 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,6 @@ export default defineConfig({
   ],
   build: {
     manifest: "manifest.json", // gera manifest.json na raiz do outDir
-    outDir: "public/assets", // onde os arquivos são gerados fisicamente
     emptyOutDir: true,
     copyPublicDir: false,
     assetsDir: "", // <<< impede a duplicação /assets/assets
