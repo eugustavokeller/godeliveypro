d0d8cd61621df302d1d1bb1ae86cedbab52bb944
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2025-10-27 22:14:11 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/assets/",
+  base: "/",
   plugins: [
     laravel({
       input: ["resources/js/app.js", "resources/css/app.css"],
@@ -16,7 +16,7 @@ export default defineConfig({
   ],
   build: {
     manifest: "manifest.json", // gera manifest.json na raiz do outDir
-    outDir: "public/build", // onde os arquivos são gerados fisicamente
+    outDir: "public/assets", // onde os arquivos são gerados fisicamente
     emptyOutDir: true,
     copyPublicDir: false,
     assetsDir: "", // <<< impede a duplicação /assets/assets
