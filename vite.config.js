ec87cb6885c9516ee762012f81390f6b64ad0bd2
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2025-10-27 21:31:27 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/assets/",
+  base: "/build/",
   plugins: [
     laravel({
       input: ["resources/js/app.js", "resources/css/app.css"],
